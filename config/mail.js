8562dde7082e5d837e4884e2bab772c7df5c8a00
march-dave <ysangy@gmail.com>
2016-06-19 00:15:58 -0700

    code clean up

--- config/mail.js
+++ config/mail.js
@@ -16,21 +16,5 @@ exports.sendVerify = () => {
   request.path = '/v3/mail/send';
   request.body = requestBody;
   sg.API(request, function (response) {
-    // console.log(response.statusCode);
-    // console.log(response.body);
-    // console.log(response.headers);
   })
-
-  // var sendgrid  = require('sendgrid').SendGrid(process.env.SENDGRID_JMS_API_KEY);
-  //
-  // sendgrid.send({
-  //   to:       'example@example.com',
-  //   from:     'other@example.com',
-  //   subject:  'Hello World',
-  //   text:     'My first email through SendGrid.'
-  // }, function(err, json) {
-  //   if (err) { return console.error(err); }
-  //   console.log(json);
-  // });
-
 };
