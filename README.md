7c656a5de1b6f60e0c1dc1c71ebd5ef695d7dddf
march-dave <ysangy@gmail.com>
2016-06-18 23:46:54 -0700

    heroku deploy

---
+++ README.md
@@ -0,0 +1,2 @@
+https://tranquil-peak-98278.herokuapp.com/
+heroku config:add SENDGRID_JMS_API_KEY='???'
--- config/mail.js
+++ config/mail.js
@@ -1,22 +1,25 @@
 'use strict';
-// var dotenv = require('dotenv');
-
-
-// var SendGrid = require("sendgrid")("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");
-// var toEmail = 'ysangy@gmail.com';
-
-// var sendgrid = require("sendgrid")("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");
-// var email = new sendgrid.Email();
-//
-// email.addTo("test@sendgrid.com");
-// email.setFrom("you@youremail.com");
-// email.setSubject("Sending with SendGrid is Fun");
-// email.setHtml("and easy to do anywhere, even with Node.js");
-//
-// sendgrid.send(email);
-
 
 exports.sendVerify = function() {
+
+  let helper = require('sendgrid').mail;
+  let from_email = new helper.Email("ysangy@gmail.com");
+  let to_email = new helper.Email("ysangy@gmail.com");
+  let subject = "I am subject";
+  let content = new helper.Content("text/plain", "I am text");
+  let mail = new helper.Mail(from_email, subject, to_email, content);
+
+  let sg = require('sendgrid').SendGrid(process.env.SENDGRID_JMS_API_KEY);
+  let requestBody = mail.toJSON();
+  let request = sg.emptyRequest();
+  request.method = 'POST';
+  request.path = '/v3/mail/send';
+  request.body = requestBody;
+  sg.API(request, function (response) {
+    // console.log(response.statusCode);
+    // console.log(response.body);
+    // console.log(response.headers);
+  })
 
   // var sendgrid  = require('sendgrid').SendGrid(process.env.SENDGRID_JMS_API_KEY);
   //
@@ -29,24 +32,5 @@ exports.sendVerify = function() {
   //   if (err) { return console.error(err); }
   //   console.log(json);
   // });
-
-  let helper = require('sendgrid').mail;
-  let from_email = new helper.Email("ysangy@gmail.com");
-  let to_email = new helper.Email("ysangy@gmail.com");
-  let subject = "subject";
-  let content = new helper.Content("text/plain", "some text here");
-  let mail = new helper.Mail(from_email, subject, to_email, content);
-
-  let sg = require('sendgrid').SendGrid(process.env.SENDGRID_JMS_API_KEY);
-  let requestBody = mail.toJSON();
-  let request = sg.emptyRequest();
-  request.method = 'POST';
-  request.path = '/v3/mail;/send';
-  request.body = requestBody;
-  sg.API(request, function (response) {
-    console.log(response.statusCode);
-    console.log(response.body);
-    console.log(response.headers);
-  })
 
 };
