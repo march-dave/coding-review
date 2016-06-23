1b9f1cf738933e8c11cde801c20f7df8009e55b8
march-dave <ysangy@gmail.com>
2016-06-23 10:17:59 -0700

    javascript challenge add

--- challenge.js
+++ challenge.js
@@ -1,3 +1,9 @@
+/*
+  Javascript bind, call, apply usages how to
+  call parameter will be comma seperate type
+  apply parameter will be array type
+*/
+
 var hero = {
 
   _name : "John Doe",
@@ -11,6 +17,6 @@ var stolen = hero.getSecretIdentify.bind(hero);
 
 console.log('stolen: ', stolen);
 console.log('stolen(): ', stolen() );
-// console.log('stolen.call(): ', stolen.call(hero) );
-// console.log('stolen.call(): ', stolen.apply(hero) );
+console.log('stolen.call(): ', stolen.call(hero) );
+console.log('stolen.call(): ', stolen.apply(hero) );
 console.log('hero.getSecretIdentify(): ', hero.getSecretIdentify());
