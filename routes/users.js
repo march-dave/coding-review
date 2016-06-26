f76100a0a349d433147f8af553aaac14cc3f2285
march-dave <ysangy@gmail.com>
2016-06-26 16:33:24 -0700

    up to date

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log('response with a resoucre console.log');
+  res.send('respond with a resource');
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req.body);
   res.send('respond with a resource');
 });
 
