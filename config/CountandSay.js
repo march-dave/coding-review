240eb8bd216465dda44af21651f3a671a74cb1f6
march-dave <ysangy@gmail.com>
2016-09-04 15:43:35 -0400

    javascript practice Count and Say

--- config/CountandSay.js
+++ config/CountandSay.js
@@ -5,4 +5,27 @@
  */
 var countAndSay = function(n) {
 
+  var startNum = 0;
+  var endNum = 0;
+
+  var str = n.toString();
+
+  var retVal = '';
+
+  for(var i=0; i<str.length; i++) {
+
+    // compare start number and end number
+    startNum = str[0];
+
+    if (startNum != str[i]) {
+      endNum = str[i];
+    }
+
+    retVal = str[i] + str.length;
+    console.log('retVal', retVal);
+  }
 };
+
+countAndSay(1);
+
+// 1, 11, 21, 1211, 111221,
